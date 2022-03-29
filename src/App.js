b1d56acde5064750c211c9cb6fb1b8b475113620
Jaden <hoichun1209@gmail.com>
2022-03-30 07:38:18 +0800

    Set up React and TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-center m-4 text-2xl font-bold text-slate-500 border-2 px-4 py-2 rounded-full shadow-md'>ClothesToGather</h1>
+    </div>
+  );
+}
+
+export default App;
