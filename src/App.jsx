c0ef8cbab797b405e4763c72439c9e2259cec41d
Jaden <hoichun1209@gmail.com>
2022-04-04 07:27:35 +0800

    ItemCard and ItemCardCollection components 🏷

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ItemCardCollection from './component/ItemCardCollection';
+
+function App() {
+  // return (
+  //   <div className='flex h-screen items-center justify-center'>
+  //     <h1 className='text-center m-4 text-2xl font-bold text-slate-500 border-2 px-4 py-2 rounded-full shadow-md'>ClothesToGather</h1>
+  //   </div>
+  // );
+  return <ItemCardCollection />
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '450px',
+      },
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
